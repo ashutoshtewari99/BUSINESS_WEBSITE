1943534de702f5cd898e69d2f191ca72bfe3b09d
ASHUTOSH TEWARI <54748312+ashutoshtewari99@users.noreply.github.com>
2021-05-10 09:01:30 +0530

    Update

--- script.js
+++ script.js
@@ -1,2 +1,8 @@
 document.querySelector(".hamburger-menu").addEventListener("click", () => { document.querySelector(".container").classList.toggle("change");
 });
+ window.onload = () =>
+ {
+     setTimeout(() => {
+         document.querySelector('body').classList.add('display')
+     }, 2000)
+ }
